c7f8547c5225159417d70ad08662d8cc73d3d813
ayushsatyam146 <ayushsatyam146@gmail.com>
2022-06-05 23:34:31 +0530

    configuring a basic /register POST api

--- auth.js
+++ auth.js
@@ -1,11 +1,25 @@
 const router = require('express').Router();
+const User = require('./models/User');
 
-router.post('/register', (req, res) => {
+router.post('/test', (req, res) => {
     res.send('registering user');
 });
 
-router.get('/register', async (req, res) => {
-    res.send(await JSON.stringify(await req.db.collection('users').find({}).toArray()));
+router.post('/register', async (req, res) => {
+    const user = new User({
+        first_name : req.body.first_name,
+        last_name : req.body.last_name,
+        email : req.body.email,
+        password : req.body.password,
+        avatar : req.body.avatar
+    });
+    try{
+        const savedUser = await user.save();
+        res.send({user: user._id});
+    }catch(err){
+        res.status(500).send(err);
+    }
 });
+
 
 module.exports = router;
--- index.js
+++ index.js
@@ -5,6 +5,9 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+//Importing the routes
+const authRoutes = require('./auth');
+
 //Connect to DB
 mongoose.connect(
     process.env.MONGODB_URI,
@@ -18,7 +21,10 @@ mongoose.connect(
     }
 )
 
-//Importing the routes
-const authRoutes = require('./auth');
+// Middlewares
+app.use(express.json());
+
+
+//Route Middlewares
 app.use('/api/user', authRoutes);
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
